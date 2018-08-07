2f373cf509b1944c8efba85c30f7fda074f61ee0
yk_de <1551332434@qq.com>
2018-08-07 15:58:49 +0800

    1.启动页由main配置，app默认不再配置default activity 2.基本完成登录功能 3.组件间提供fragment测试ok

--- commonLibrary/src/main/java/com/tengbo/commonlibrary/common/ComponentConfig.java
+++ commonLibrary/src/main/java/com/tengbo/commonlibrary/common/ComponentConfig.java
@@ -7,6 +7,10 @@ public class ComponentConfig {
         public static final String COMPONENT_NAME = "ComponentOrder";
 
         public static final String ACTION_GET_HISTORY_FRAGMENT="getHistoryFragment";
+
+        public static final String ACTION_GET_TASK_FRAGMENT="getTaskFragment";
+
+        public static final String ACTION_GET_PROCESSING_FRAGMENT="getProcessingFragment";
     }
 
     // 个人中心组件的配置项
